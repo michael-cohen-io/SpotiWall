0db17c6b44199a4f47b10c19e497a86e6513558b
mickeyjk <micohen13@gmail.com>
2016-05-11 22:57:13 -0400

    Initial Commit

---
+++ SpotiWall/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  SpotiWall
+//
+//  Created by Michael Cohen on 5/11/16.
+//  Copyright © 2016 Michael Cohen. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
